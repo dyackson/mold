fb2e5ed0d2859e473ff87c2dbfeacefbb908adbe
Jackson Millsaps <jacksonkmillsaps@gmail.com>
2023-08-09 14:25:23 +0100

    inlines some int functions

--- lib/anal/int.ex
+++ lib/anal/int.ex
@@ -16,15 +16,61 @@ defmodule Anal.Int do
 
   defimpl Anal do
     def prep!(%Spec{} = spec) do
+      spec = Common.prep!(spec)
+
+      bad_bound =
+        case spec do
+          %{lt: lt} when not (lt == nil or is_integer(lt)) ->
+            :lt
+
+          %{lte: lte} when not (lte == nil or is_integer(lte)) ->
+            :lte
+
+          %{gt: gt} when not (gt == nil or is_integer(gt)) ->
+            :gt
+
+          %{gte: gte} when not (gte == nil or is_integer(gte)) ->
+            :gte
+
+          _ ->
+            nil
+        end
+
+      if bad_bound, do: raise(SpecError.new("#{inspect(bad_bound)} must be an integer"))
+
+      if is_integer(spec.gt) and is_integer(spec.gte),
+        do: raise(SpecError.new("cannot use both :gt and :gte"))
+
+      if is_integer(spec.lt) and is_integer(spec.lte),
+        do: raise(SpecError.new("cannot use both :lt and :lte"))
+
+      impossible_bounds =
+        case spec do
+          %{gt: gt, lt: lt} when is_integer(gt) and is_integer(lt) and gt >= lt ->
+            {:gt, :lt}
+
+          %{gt: gt, lte: lte} when is_integer(gt) and is_integer(lte) and gt >= lte ->
+            {:gt, :lte}
+
+          %{gte: gte, lt: lt} when is_integer(gte) and is_integer(lt) and gte >= lt ->
+            {:gte, :lt}
+
+          %{gte: gte, lte: lte} when is_integer(gte) and is_integer(lte) and gte >= lte ->
+            {:gte, :lte}
+
+          _ ->
+            nil
+        end
+
+      case impossible_bounds do
+        {lower, upper} ->
+          raise SpecError.new("#{inspect(lower)} must be less than #{inspect(upper)}")
+
+        nil ->
+          nil
+      end
+
       spec
-      |> Common.prep!()
-      |> check_integer_or_nil!(lt: spec.lt)
-      |> check_integer_or_nil!(gt: spec.gt)
-      |> check_integer_or_nil!(lte: spec.lte)
-      |> check_integer_or_nil!(gte: spec.gte)
-      |> at_most_one!(lt: spec.lt, lte: spec.lte)
-      |> at_most_one!(gt: spec.gt, gte: spec.gte)
-      |> ensure_logical_bounds!()
       |> add_error_message()
       |> Map.put(:__prepped__, true)
     end
@@ -32,13 +78,21 @@ defmodule Anal.Int do
     def exam(%Spec{} = spec, val) do
       spec = Common.check_prepped!(spec)
 
-      with :not_nil <- Common.exam_nil(spec, val),
-           :ok <- local_exam(spec, val),
-           :ok <- Common.apply_also(spec, val) do
-        :ok
-      else
-        :ok -> :ok
-        :error -> {:error, spec.error_message}
+      case {spec.nil_ok?, val} do
+        {true, nil} ->
+          :ok
+
+        {false, nil} ->
+          {:error, spec.error_message}
+
+        _ ->
+          # check the non-nil value with the spec
+          with :ok <- local_exam(spec, val),
+               :ok <- Common.apply_also(spec, val) do
+            :ok
+          else
+            :error -> {:error, spec.error_message}
+          end
       end
     end
 
@@ -51,49 +105,6 @@ defmodule Anal.Int do
         :ok
       else
         _ -> :error
-      end
-    end
-
-    defp check_integer_or_nil!(%Spec{} = spec, [{_key, val}]) when is_integer(val) or is_nil(val),
-      do: spec
-
-    defp check_integer_or_nil!(%Spec{}, [{key, _val}]),
-      do: raise(SpecError.new("#{inspect(key)} must be an integer"))
-
-    defp at_most_one!(%Spec{} = spec, [{key1, val1}, {key2, val2}]) do
-      if is_nil(val1) || is_nil(val2) do
-        spec
-      else
-        raise SpecError.new("cannot use both #{inspect(key1)} and #{inspect(key2)}")
-      end
-    end
-
-    defp ensure_logical_bounds!(%Spec{} = spec) do
-      # at this point, at_most_one/3 hessage ensured there is at most one lower or upper bound
-      lower_bound_tuple =
-        case {spec.gt, spec.gte} do
-          {nil, nil} -> nil
-          {gt, nil} -> {:gt, gt}
-          {nil, gte} -> {:gte, gte}
-        end
-
-      upper_bound_tuple =
-        case {spec.lt, spec.lte} do
-          {nil, nil} -> nil
-          {lt, nil} -> {:lt, lt}
-          {nil, lte} -> {:lte, lte}
-        end
-
-      case {lower_bound_tuple, upper_bound_tuple} do
-        {l, u} when is_nil(l) or is_nil(u) ->
-          spec
-
-        {{lower_k, lower_v}, {upper_k, upper_v}} ->
-          if lower_v < upper_v do
-            spec
-          else
-            raise SpecError.new("#{inspect(lower_k)} must be less than #{inspect(upper_k)}")
-          end
       end
     end
 
