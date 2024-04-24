8aa9d3419bea1df177e8a346c2474d94cb358043
Jackson Millsaps <jacksonkmillsaps@gmail.com>
2024-04-24 20:16:39 +0100

    update elixir

--- mix.exs
+++ mix.exs
@@ -3,9 +3,9 @@ defmodule Mold.MixProject do
 
   def project do
     [
-      app: :anal,
+      app: :mold,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
